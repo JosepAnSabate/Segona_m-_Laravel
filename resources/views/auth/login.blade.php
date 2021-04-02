3321281d4431b5e80a8eac0759e53f35b70d25e3
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-04-02 03:00:11 +0200

    sf

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,65 @@
+
+@extends('layouts.app')  
+@section('content') 
+<!-- ======= Book A Table Section ======= -->
+<section id="book-a-table" class="book-a-table">
+    <div class="container">
+      <div class="section-title">
+        <h2>Haz Login a<span> Beercraft</span></h2>
+        <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+      </div>
+      @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+  @endif
+      <form action="/login" method="POST" role="form" class="php-email-form">
+        @csrf
+        <div class="row">
+          {{-- <div class="col-md-6 form-group">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Nombre Cerveceria" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+            <div class="validate"></div>
+          </div> --}}
+          <div class="col-md-6 offset-md-3 form-group mt-3 mt-md-0">
+            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email">
+            <div class="validate"></div>
+          </div>
+          <div class="col-md-6 offset-md-3 form-group mt-3 mt-md-0">
+            <input type="password" class="form-control" name="password" id="password" placeholder="Your Password">
+            <div class="validate"></div>
+          </div>
+          {{-- <div class="col-lg-4 col-md-6 form-group mt-3 mt-md-0">
+            <input type="text" class="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+            <div class="validate"></div>
+          </div>
+          <div class="col-lg-4 col-md-6 form-group mt-3">
+            <input type="text" name="date" class="form-control" id="date" placeholder="Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+            <div class="validate"></div>
+          </div>
+          <div class="col-lg-4 col-md-6 form-group mt-3">
+            <input type="text" class="form-control" name="time" id="time" placeholder="Time" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+            <div class="validate"></div>
+          </div>--}}
+          {{-- <div class="col-md-6 form-group">
+            <input type="number" class="form-control" name="capacity" id="people" placeholder="# de personas" data-rule="minlen:1" data-msg="Please enter at least 1 chars">
+            <div class="validate"></div>
+          </div>  --}}
+        </div>
+        {{-- <div class="form-group mt-3">
+          <textarea class="form-control" name="description" rows="5" placeholder="Descripción"></textarea>
+          <div class="validate"></div>
+        </div> --}}
+        <div class="mb-3">
+          <div class="loading">Loading</div>
+          <div class="error-message"></div>
+          <div class="sent-message">Your booking request was sent. We will call back or send an Email to confirm your reservation. Thank you!</div>
+        </div>
+        <div class="text-center"><button type="submit">Entrar</button></div>
+      </form>
+    </div>
+  </section><!-- End Book A Table Section -->
+@endsection
---
+++ resources/views/layouts/_nav.blade.php
@@ -0,0 +1,57 @@
+
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg text-uppercase py-0 border-bottom" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger text-decoration-none text-reset prontologo"
+            href="{{ url('/') }}">Segmano</a>
+        <button
+            class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
+            type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
+            aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown py-2">
+                    <a id="navbarDropdown"
+                        class=" nav-link text-lowercase dropdown-toggle text-decoration-none text-reset" href="#"
+                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <span>Categorias</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item text-decoration-none text-reset"
+                            href="#">
+                        </a>
+                    </div>
+                </li>
+                <li class="nav-item mx-0 mx-lg-1 "><a
+                        class=" borderMarcador nav-link text-lowercase py-3 px-0 px-lg-3 rounded js-scroll-trigger text-decoration-none text-reset"
+                        href="#"><span>Nuevo artículo</span></a></li>
+                @guest
+                @if (Route::has('login'))
+                <li class="nav-item mx-0 mx-lg-1 ">
+                    <a class="borderMarcador nav-link text-lowercase py-3 px-0 px-lg-3 rounded js-scroll-trigger text-decoration-none text-reset"
+                        href="{{route('login')}}"><span>Login</span></a>
+                </li>
+                @endif    
+                @if (Route::has('register'))
+                <li class="nav-item mx-0 mx-lg-1">
+                    <a class="borderMarcador nav-link text-lowercase py-3 px-0 px-lg-3 rounded js-scroll-trigger text-decoration-none text-reset"
+                        href="{{route('register')}}"><span>Register</span></a>
+                </li>
+                @endif
+                @else
+                <li class="nav-item mx-0 mx-lg-1">
+                    <form id="logoutForm" action="{{route('logout')}}" method="POST">
+                        @csrf
+                    </form>
+                    <a id="logoutBtn"
+                        class=" nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger text-decoration-none text-reset"
+                        href="#">Logout</a>
+                </li>
+ @endguest
+    </ul>
+        </div>
+    </div>
+</nav>
