ba7f94a42165a4e34f96e952606710b955156f71
nicomil <nicombk@gmail.com>
2021-04-14 12:13:28 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,11 +28,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {  
-        if(Schema::hasTable('categories')){
-
-            $categories = Category::all();
-            View::share('categories', $categories);
-          }    
+          
         Paginator::useBootstrap();
     }
 }
